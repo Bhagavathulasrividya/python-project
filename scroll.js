0c54660447b8d2197053d1b6f1670a466c7ed8fc
Bhagavathulasrividya <srividya12june@gmail.com>
2025-11-07 11:19:13 +0530

    uploading gsap files

---
+++ scroll.js
@@ -0,0 +1,26 @@
+// gsap.from("#page2 h1",{
+//     scale:0.5,
+//     opacity:0,
+//     duration:2,
+//     delay:1,
+//    scrollTrigger:"#page2 h1"
+// })
+
+gsap.from("#page2 h1",{
+    x:-800,
+    y:300,
+     rotate:265,
+     scrollTrigger:"#page2 h1"
+})
+gsap.from("#page2 img",{
+     x:800,
+     y:400,
+     rotate:360,
+     scale:0.5,
+     duration:1,
+     scrollTrigger:"#page2 h1"
+     })
+
+   
+
+
